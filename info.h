0b6be081bfbb0d97817a30c756a480c7c1eb866d
Hiroaki Fukuda <hiroaki@pine.local>
2017-02-16 13:10:06 +0900

    add info.h

---
+++ info.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   info.h
+ * Author: hiroaki
+ *
+ * Created on February 16, 2017, 8:08 AM
+ */
+
+#ifndef _INFO_H_
+#define _INFO_H_
+
+#include "mas.h"
+
+struct LocalInfo_tag {
+    int line_number;
+};
+
+#endif /* INFO_H */
+
