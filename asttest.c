cc9ba73b18a30fdc7f874abf4cfef6feb22364b3
Hiroaki Fukuda <hiroaki@pine.local>
2017-02-19 12:32:59 +0900

    easy ast test finished

--- asttest.c
+++ asttest.c
@@ -9,10 +9,20 @@
 static void show(Expression* expr) {
     switch(expr->type) {
         case ADD_EXPRESSION:
+            fprintf(stderr, "add\n");
+            show(expr->u.binary_expression.left);
+            show(expr->u.binary_expression.right);
+            break;
+        case SUB_EXPRESSION:
+            fprintf(stderr, "sub\n");
+            show(expr->u.binary_expression.left);
+            show(expr->u.binary_expression.right);
             break;
         case INT_EXPRESION:
+            fprintf(stderr, "int:%d\n", expr->u.int_value);
             break;
         case DOUBLE_EXPRESSION:
+            fprintf(stderr, "double:%f\n", expr->u.double_value);
             break;
         default:
             break;
@@ -25,7 +35,14 @@ int main(void) {
     MAS_Interpreter *interp = mas_create_interpreter();
     Expression* int_expr = mas_create_int_expression(10);
     Expression* d_expr = mas_create_double_expression(3.5);
-    Expression* bexpr = mas_create_binary_expression(ADD_EXPRESSION, int_expr, d_expr);
+//    Expression* bexpr = mas_create_binary_expression(ADD_EXPRESSION, int_expr, d_expr);
+    
+    Expression* int_expr2 = mas_create_int_expression(1);
+    Expression* int_expr3 = mas_create_int_expression(2);
+    Expression* bexpr2 = mas_create_binary_expression(SUB_EXPRESSION, int_expr2, int_expr3);
+    
+    Expression* bexpr = mas_create_binary_expression(ADD_EXPRESSION, bexpr2, d_expr);  
+    
     show(bexpr);
     
     mas_delete_interpreter();
